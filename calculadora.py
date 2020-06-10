9810d128ea6f14c574cd19ee74d08391bfe4621a
Murilo Moraes <murilo.alves8@gmail.com>
2020-06-10 11:20:38 -0300

    Redução de script

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,21 @@
 git remote add origin https://github.com/Moraes97-Murilo/CursoGit.gii
 
 op = input("Você gostaria de somar, subtrair, multiplicar ou dividir? ")
+a = float (input ("Digite o primeiro valor: ")) 
+b = float (input ("Digite o segundo valor: ")) 
+
 if op == "somar":
-    a = float (input ("Digite o primeiro valor: ")) 
-    b = float (input ("Digite o segundo valor: ")) 
     print (a+b)
     
 elif op == "subtrair":
-    a = float (input ("Digite o primeiro valor: ")) 
-    b = float (input ("Digite o segundo valor: ")) 
     print (a-b)
     
 elif op == "multiplicar":
-    a = float (input ("Digite o primeiro valor: ")) 
-    b = float (input ("Digite o segundo valor: ")) 
     print (a*b)
     
 elif op == "dividir":
-    a = float (input ("Digite o primeiro valor: ")) 
-    b = float (input ("Digite o segundo valor: ")) 
     print (a/b)
     
 else:
     print("Operação desconhecida.")
+
